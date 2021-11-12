3e7fb3e1f1a1d24b8c43c4f0644731bd9907a39f
19FTT1068 <19FTT1068@student.pb.edu.bn>
2021-11-12 21:25:00 +0800

    new profile design

--- profile_test.php
+++ profile_test.php
@@ -16,6 +16,7 @@
             href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
             integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
             crossorigin="anonymous">
+            
 
         <!-- Google Fonts -->
         <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
@@ -23,7 +24,7 @@
 
         <!-- Custom Styling -->
         <link rel="stylesheet" href="assets/css/style.css">
-
+        <link rel="stylesheet" href="assets/css/prof.css">
         <!-- page Styling -->
         <!-- <link rel="stylesheet" href="assets/css/page.css"> -->
 
@@ -40,20 +41,15 @@
         <div class="page-wrapper">
 
         <div class="sidebar-wrapper">
-
         <?php include(ROOT_PATH . "/app/includes/sidebar.php"); ?>
+        </div>
 
-</div>
 
-
-                <div class="auth-content" align="center" >
+                <!-- <div class="auth-content" align="center" >
                 <?php include(ROOT_PATH . "/app/helpers/formErrors.php"); ?>
-
                 <?php include(ROOT_PATH . "/app/includes/messages.php"); ?> 
-                
-
-                    <h1 class="page-title">Welcome to your profile</h1>
-
+                    <!-- <h1 class="page-title">Welcome to your profile</h1> -->
+                </div> -->
 
                     <?php  
                     // to get user details by fetching user punya database table 
@@ -70,52 +66,78 @@
                     
                     ?>
                     
-                    
-
-                    <form action="profile.php" method="post" enctype="multipart/form-data">
-                        <input type="hidden" name="id" value="<?php echo $_SESSION['id']; ?>" >
-                       
-                        <div   >
-                        <img src="<?php echo BASE_URL . '/assets/profile/' . $pic; ?>" height="100" width="100"/> 
-                        </div>
-                        <div > 
-                            <h3>Username</h3>
-                                <p><?php echo $_SESSION['username']; ?></p>
-                        </div>
-                        
-                        <div>
-                            <h3>Email</h3>
-                          <p> <?php echo $_SESSION['email']; ?> </p>
-                        </div>
-                        <div>
-                            <h3>Phone Number</h3>
-                            <p> <?php echo $phone_number; ?> </p>
-                        </div>
-                        <div>
-                            <h3>Bio</h3>
-                            <p> <?php echo $_SESSION['bio']; ?> </p>
-                            <p>Gamer summoned on <?php echo date('F j, Y', strtotime($created)); ?></p>
-                        </div>
-                        <br>
-                        <div>
-                           <a href="<?php echo BASE_URL . '/edit-profile.php' ?>" class="btn btn-big">Edit Profile</a>
-
-                        </div>
-
-
-                        
-                    </form>
-                   
-                    </div>
-
-                </div>
-
-            </div>
             <!-- // page Content -->
+<div class="wrapper">
+ 
+    <div class="left">
+        <img src="<?php echo BASE_URL . '/assets/profile/' . $pic; ?>" 
+        alt="user" width="75%" height="auto">
+        <h4><?php echo $_SESSION['username']; ?></h4>
+         <p><?php echo $_SESSION['bio']; ?></p>
+
+        <!-- <div class="badges">
+        <h3>Badges</h3>
+        <img src="assets/profile_badge/b3.png" 
+        alt="user" width="13%" height="auto">
+
+        <img src="assets/profile_badge/b2.png" 
+        alt="user" width="15%" height="auto">
+
+        <img src="assets/profile_badge/b1.png" 
+        alt="user" width="15%" height="auto">
+        </div> -->
+    </div>
+
+    <div class="right">
+
+        <div class="info">
+            <h3>Information</h3>
+            <div class="info_data">
+                 <div class="data">
+                    <h4>Email</h4>
+                    <p><?php echo $_SESSION['email']; ?></p>
+                 </div>
+                 <div class="data">
+                   <h4>Phone</h4>
+                    <p><?php echo $phone_number; ?></p>
+              </div>
+            </div>
+        </div>
+      
+      <div class="projects">
+            <h3>Achievements</h3>
+            <div class="projects_data">
+                 <div class="data">
+                    <h4>Events Joined:</h4>
+                    <p>-</p>
+                 </div>
+                 <div class="data">
+                   <h4>Summoned on</h4>
+                    <p><?php echo date('F j, Y', strtotime($created)); ?></p>
+              </div>
+            </div>
+        </div>
+      
+      <div class="links">
+            <h3>Social Media</h3>
+            <ul>
+              <li><a href="https://steamcommunity.com/profiles/76561199032818871/"><i class="fab fa-steam"></i></a></li>
+              <li><a href="Zylioth#1580"><i class="fab fa-discord"></i></a></li>
+              <li><a href="https://www.instagram.com/_amir02/"><i class="fab fa-instagram"></i></a></li>
+          </ul>
+      </div>
+
+      <div class="edit">
+        <a href="<?php echo BASE_URL . '/edit-profile.php' ?>" class="btn">Edit Profile</a>
+      </div>
+
+      
+    </div>
+</div>
 
         
         <!-- // Page Wrapper -->
-
+        </div>
 
         <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 
@@ -127,7 +149,7 @@
             src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script>
         <!-- Custom Script -->
         <script src="assets/js/scripts.js"></script>
-
+        <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
         <script>
   /* Set the width of the side navigation to 250px */
 function openNav() {
