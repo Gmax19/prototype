231d8794916c9ff9f31cc048a8ff8695d40f70cc
Gmax19 <19ftt1194@student.pb.edu.bn>
2021-11-29 20:17:11 +0800

    some changes

--- admin/events/create.php
+++ admin/events/create.php
@@ -76,7 +76,10 @@ adminOnly();
                             <label>category</label>
                             <select name="category" class="text-input">
                                 <option value="Solo">Solo</option>
+                                <option value="Duo">Duo</option>
                                 <option value="Team">Team</option>
+                                <option value="Team (4 person)">Team (4 person)</option>
+                                <option value="Team (5 person)">Team (5 person)</option>
                             </select>
                         </div>
                         <div>
