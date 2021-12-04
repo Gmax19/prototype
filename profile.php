d2c79cdb6c1d16cdc692d2413216bbfd2df19cd8
19FTT1068 <19FTT1068@student.pb.edu.bn>
2021-12-04 13:37:02 +0800

    no changes

--- profile.php
+++ profile.php
@@ -66,27 +66,10 @@
                         $discord = $fetch['discord'];
                         $created = $fetch['created_at'];
                     }
-<<<<<<< HEAD
-
-                    $team_id = mysqli_query($conn,"SELECT team_id FROM team_members as tm
-                        inner join users as u on tm.member_id = u.id
-                        where u.id = $id limit 1");
-
-                    $result = mysqli_fetch_assoc($team_id);
-                    $teamid = $result['team_id'];
-
-                      $participants = "SELECT *, p.id as paymentid  FROM payments as p
-                        inner join users as u on p.user_id = u.id
-                        inner join team_members as tm on tm.member_id = u.id
-                        inner JOIN events as e on e.id = p.product_id 
-                        where u.id = $id OR p.team_id = $teamid ; ";
- 
-=======
                     $participants = "SELECT * FROM `payments` as p 
                     inner join users as u on p.user_id = u.id
                     inner JOIN events as e on e.id = p.product_id where u.id = $id";
 
->>>>>>> 17779a7d29e269e869ab663c53fc1d2fabbd0448
                     ?>
                     
             <!-- // page Content -->
